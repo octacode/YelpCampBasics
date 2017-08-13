eaa2d5153549b48e5d315795665e745d9549af47
octacode <allblue.shasha@gmail.com>
2017-08-13 17:07:01 +0530

    Added mongoose support to the site

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/yelp_camp", {
+  useMongoClient: true
+});
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+})
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Cat", image: "http://fakeimg.pl/350x200/?text=Cat&font=lobster"
+//   },function(err, campground){
+//     if(err){
+//       console.log("Something went wrong.")
+//     }
+//     else{
+//       console.log("The new campground inserted is: ");
+//       console.log(campground);
+//     }
+//   }
+// )
 
 app.use(bodyParser.urlencoded({extended: true}));
-var campgrounds = [
-  {name: "Dog", image: "http://fakeimg.pl/350x200/?text=Dog&font=lobster"},
-  {name: "Cat", image: "http://fakeimg.pl/350x200/?text=Cat&font=lobster"},
-  {name: "Lion", image: "http://fakeimg.pl/350x200/?text=Lion&font=lobster"},
-  {name: "Bat", image: "http://fakeimg.pl/350x200/?text=Bat&font=lobster"},
-  {name: "Dog", image: "http://fakeimg.pl/350x200/?text=Dog&font=lobster"},
-  {name: "Cat", image: "http://fakeimg.pl/350x200/?text=Cat&font=lobster"},
-  {name: "Lion", image: "http://fakeimg.pl/350x200/?text=Lion&font=lobster"},
-  {name: "Bat", image: "http://fakeimg.pl/350x200/?text=Bat&font=lobster"},
-  {name: "Dog", image: "http://fakeimg.pl/350x200/?text=Dog&font=lobster"},
-  {name: "Cat", image: "http://fakeimg.pl/350x200/?text=Cat&font=lobster"},
-  {name: "Lion", image: "http://fakeimg.pl/350x200/?text=Lion&font=lobster"},
-  {name: "Bat", image: "http://fakeimg.pl/350x200/?text=Bat&font=lobster"},
-  {name: "Dog", image: "http://fakeimg.pl/350x200/?text=Dog&font=lobster"},
-  {name: "Cat", image: "http://fakeimg.pl/350x200/?text=Cat&font=lobster"},
-  {name: "Lion", image: "http://fakeimg.pl/350x200/?text=Lion&font=lobster"},
-  {name: "Bat", image: "http://fakeimg.pl/350x200/?text=Bat&font=lobster"}]
 
 app.get('/',function(req,res){
     res.render('landing.ejs');
 });
 
 app.get('/campgrounds', function(req,res){
-  res.render('campground.ejs', {campgrounds: campgrounds});
+  Campground.find({}, function(err, campgrounds){
+      if(err){
+        console.log("ERROR fetching from data base.");
+      }
+      else{
+        res.render('campground.ejs', {campgrounds: campgrounds});
+      }
+  });
 });
 
 app.get('/campgrounds/new', function(req,res){
@@ -36,9 +52,17 @@ app.get('/campgrounds/new', function(req,res){
 app.post('/campgrounds', function(req,res){
    var name = req.body.name;
    var image = req.body.image;
-   campgrounds.push({name: name, image: image});
-   res.render('campground.ejs', {campgrounds: campgrounds})
-  res.send("you hit the post route");
+
+   Campground.create({
+     name: name,
+     image: image
+   }, function(err, campground){
+      if(err)
+        console.log("Error while inserting!");
+      else{
+        res.redirect('/campgrounds');
+      }
+   });
 });
 
 app.listen(1313, function(){
