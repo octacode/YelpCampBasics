c61d160d83c40a168dcbdd10e535932619ef421b
octacode <allblue.shasha@gmail.com>
2017-08-18 04:44:47 +0530

    Added register and login skeleton

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     Campground = require("./models/campgroundModel.js"),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
     seedDb = require("./seeds.js"),
+    User = require("./models/user.js"),
     Comment = require("./models/comment.js");
 
 mongoose.connect("mongodb://localhost/yelp_camp", {
@@ -14,6 +17,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 seedDb();
 
+//=================
+//PASSPORT CONFIGURATION
+//=================
+app.use(require('express-session')({
+  secret: "I just want to be rich.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//==================
+//ROUTES
+//==================
 app.get('/',function(req,res){
     res.render('landing.ejs');
 });
@@ -92,6 +112,40 @@ app.post('/campgrounds/:id/comments', (req,res)=>{
     }
   });
 });
+
+
+//================
+//REGISTER ROUTES
+//================
+app.get('/register', (req,res)=>{
+  res.render('authentication/register.ejs');
+});
+
+app.post('/register', (req,res)=>{
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user)=>{
+    if(err){
+        console.log(err);
+        res.redirect('/');
+    }
+    passport.authenticate("local")(req,res, function(){
+          res.redirect('/campgrounds');
+    });
+  });
+});
+
+//==================
+//LOGIN ROUTES
+//==================
+app.get('/login', (req,res)=>{
+    res.render('authentication/login.ejs');
+});
+
+app.post('/login', passport.authenticate("local",
+        {
+          successRedirect: '/campgrounds',
+          failureRedirect: '/login'
+        }),(req,res)=>{});
 
 app.listen(1313, function(){
   console.log('Yelp Camp server sunn rha hai...')
--- models/comment.js
+++ models/comment.js
@@ -3,5 +3,4 @@ var commentSchema = new mongoose.Schema({
   author: String,
   text: String
 });
-
 module.exports = mongoose.model("Comment", commentSchema);
